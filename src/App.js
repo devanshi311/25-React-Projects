d968ad0d9d10744c370b4abe0a0c40e2f82af308
devanshi <devanshigodhaniya311@gmail.com>
2024-11-09 06:34:46 +0530

    done with accordian project

--- src/App.js
+++ src/App.js
@@ -8,6 +8,6 @@ function App() {
      <Accordion/>
     </div>
   );
-}
+} 
 
 export default App;
--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,34 +1,64 @@
 import React, { useState } from "react";
 import data from "./data";
+import './styles.css';
 
 export default function Accordion() {
     const [selected, setSelected] = useState(null);
-
+    const [enabledMultiSelection, setEnabledMultiSelection] = useState(false);
+    const [multiple , setMultiple] = useState([]);
+    
     function handleSingleSelection(getCurrentId) {
-        setSelected(getCurrentId);
-        console.log(getCurrentId);
-        
+        setSelected(getCurrentId === selected ? null : getCurrentId);
     }
-    console.log(selected)
+    
+    function handleMultiSelection(getCurrentId) {
+        let cpyMultiple = [...multiple];
+        const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+
+        if(findIndexOfCurrentId === -1) {
+            cpyMultiple.push(getCurrentId);
+        } else {
+            cpyMultiple.splice(findIndexOfCurrentId, 1);
+        }
+
+        setMultiple(cpyMultiple);
+    }
+
+    function toggleMultiSelection() {
+        setEnabledMultiSelection(!enabledMultiSelection);
+        if (enabledMultiSelection) {
+            setMultiple([]); // Reset multi-selection when disabling
+        }
+    }
+    
     return (
         <div className="wrapper">
+            <button onClick={toggleMultiSelection}>
+                {enabledMultiSelection ? "Disable Multi Selection" : "Enable Multi Selection"}
+            </button>
             <div className="accordion">
                 {data && data.length > 0 ? (
                     data.map((dataItem, index) => (
                         <div className="item" key={index}>
                             <div
-                                onClick={() => handleSingleSelection(dataItem.id)}
+                                onClick={
+                                    enabledMultiSelection
+                                     ? () => handleMultiSelection(dataItem.id)
+                                     : () => handleSingleSelection(dataItem.id)
+                                }
                                 className="title"
                             >
                                 <h3>{dataItem.question}</h3>
                                 <span>+</span>
                             </div>
                             {
-                                selected === dataItem.id ? 
-                                    <div className="content">{dataItem.answer}
-                                    </div>
-                                    : null
-                                
+                                enabledMultiSelection 
+                                ? multiple.indexOf(dataItem.id) !== -1 && (
+                                    <div className="content">{dataItem.answer}</div>
+                                  )
+                                : selected === dataItem.id && (
+                                    <div className="content">{dataItem.answer}</div>
+                                  )
                             }
                         </div>
                     ))
@@ -39,4 +69,3 @@ export default function Accordion() {
         </div>
     );
 }
-  
